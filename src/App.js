409c8f70187eff5ddfe6d04fa1898de9681de9d0
DitoT <dimitritutberidze@gmail.com>
2025-12-26 00:32:12 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import shawarmaPepper from "./images/shawarmaPepper.png";
+import girlWithBubble from "./images/girlWithBubble.jpg";
 
 function App() {
+  const [clicked, setClicked] = useState(false);
+
+  useEffect(() => {
+    if (clicked) {
+      const timer = setTimeout(() => {
+        window.location.reload();
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [clicked]);
+
+  // Generate 100 confetti/firework pieces
+  const fireworks = Array.from({ length: 100 }, () => ({
+    left: Math.random() * 100 + "%",
+    size: 5 + Math.random() * 5 + "px",
+    color: `hsl(${Math.random() * 360}, 100%, 50%)`,
+    duration: 1 + Math.random() * 2 + "s",
+    delay: Math.random() * 0.5 + "s",
+    rotate: Math.random() * 360 + "deg",
+  }));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!clicked ? (
+        <div className="container">
+          <img src={shawarmaPepper} alt="Shawarma Pepper" className="shawarma" />
+          <div className="girl-container">
+            <img src={girlWithBubble} alt="Girl" className="girl" />
+            <div className="bubble">😊</div>
+          </div>
+          <button className="blue-button" onClick={() => setClicked(true)}>
+            Click Me!
+          </button>
+        </div>
+      ) : (
+        <div className="celebration">
+          <h1 className="message">სალომე მაგარი მგელი ხარ keep going</h1>
+          {fireworks.map((fw, index) => (
+            <div
+              key={index}
+              className="firework-piece"
+              style={{
+                left: fw.left,
+                width: fw.size,
+                height: fw.size,
+                backgroundColor: fw.color,
+                animationDuration: fw.duration,
+                animationDelay: fw.delay,
+                transform: `rotate(${fw.rotate})`,
+              }}
+            ></div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
